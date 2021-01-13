3379e440358015e941e1c6f555d572877b47b199
simpleprahalad <48152622+simpleprahalad@users.noreply.github.com>
2021-01-14 05:16:50 +0530

    Added code for showing signin modal

--- scripts/index.js
+++ scripts/index.js
@@ -14,3 +14,25 @@ function hideSignUpModal() {
     var modal = document.getElementById("signup-modal");
     modal.style.display = "none";
 }
+
+function showSignInModal() {
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
+
+    var modal = document.getElementById("signin-modal");
+    modal.style.display = "block";
+}
+
+function hideSignInModal() {
+    var modal = document.getElementById("signin-modal");
+    modal.style.display = "none";
+}
+
+function handleSignUpClick() {
+    hideSignInModal();
+    showSignUpModal();
+}
