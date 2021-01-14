3e719314b719a946c50e2bd087655c73986e0b64
simpleprahalad <48152622+simpleprahalad@users.noreply.github.com>
2021-01-14 07:36:29 +0530

    added empty blogpost.html and modal for creating new post

--- scripts/index.js
+++ scripts/index.js
@@ -36,3 +36,26 @@ function handleSignUpClick() {
     hideSignInModal();
     showSignUpModal();
 }
+
+function navigateToBlogPost() {
+    location.href = "html/bloglist.html"
+}
+
+
+function showCreatePostModal() {
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
+
+    var modal = document.getElementById("create-post-modal");
+    modal.style.display = "block";
+}
+
+function hideCreatePostModal() {
+    var modal = document.getElementById("create-post-modal");
+    modal.style.display = "none";
+}
+
